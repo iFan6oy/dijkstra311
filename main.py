647feccf48374b7d0630eee56dc6ee2994df488e
Norman Cooke <nrmn.cooke@gmail.com>
2021-04-20 15:28:23 -0500

    add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# CSC311
+# HW3 - Dijkstra's Algorithm
+# Group #... what group number are we?
+
+def main():
+    print('CSC311 - Group Project - Dijkstra\'s Algorithm\n')
+
+
+# Init main
+if __name__ == '__main__':
+    main()
